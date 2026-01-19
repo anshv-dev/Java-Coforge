bfb7e2e7e336571e22657fb31aa61b1807b7092e
anshv-dev <anshv8166@gmail.com>
2026-01-19 12:36:52 +0530

    19/01/2026

---
+++ Project_Jan_2026/src/test/java/pac1/TC_FileWrite.java
@@ -0,0 +1,23 @@
+package pac1;
+ 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class TC_FileWrite {
+ 
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Text:");
+		
+		String text=sc.nextLine();
+		FileWriter fw=new FileWriter("C:\\Users\\Ansh.Verma\\Desktop\\gitfiles\\wow.txt");
+		fw.write(text);
+		fw.close();
+		
+ 
+	}
+ 
+}
---
+++ Project_Jan_2026/src/test/java/pac1/TC_HashSet.java
@@ -0,0 +1,18 @@
+package pac1;
+
+import java.util.HashSet;
+
+public class TC_HashSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        HashSet<Integer>numbers=new HashSet<>();
+        numbers.add(10);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(11);
+        System.out.println(numbers);
+	}
+
+}
