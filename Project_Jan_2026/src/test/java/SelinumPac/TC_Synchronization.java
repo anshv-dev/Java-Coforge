4af52ccbd85747a37f9bae385c917697b2368edc
anshv-dev <anshv8166@gmail.com>
2026-01-23 16:24:36 +0530

    23/01/2026

---
+++ Project_Jan_2026/src/test/java/SelinumPac/TC_Synchronization.java
@@ -0,0 +1,72 @@
+//package SelinumPac;
+// 
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+// 
+//import io.github.bonigarcia.wdm.WebDriverManager;
+// 
+//public class TC_Synchronization {
+// 
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+// 
+//		
+////		WebDriverManager.chromedriver().setup();
+////		WebDriver driver=new ChromeDriver();
+////		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+////		
+////		driver.findElement(By.name("username")).sendKeys("Admin");
+////		driver.findElement(By.name("password")).sendKeys("admin123");
+////		driver.findElement(By.xpath("//button[@type='submit']")).click();
+//		WebDriverManager.chromedriver().setup();
+//		WebDriver driver=new ChromeDriver();
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//		
+//		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+//		//Thread.sleep(5000);
+//		driver.findElement(By.name("username")).sendKeys("Admin");
+//		driver.findElement(By.name("password")).sendKeys("admin123");
+//		driver.findElement(By.xpath("//button[@type='submit']")).click();
+//		
+//	}
+// 
+//}
+
+
+package SelinumPac;
+ 
+import java.time.Duration;
+ 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+ 
+public class TC_Synchronization {
+ 
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+ 
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		//Thread.sleep(5000);
+		WebElement username=wait.until(ExpectedConditions.elementToBeClickable(By.name("username")));
+		
+		//driver.findElement(By.name("username")).sendKeys("Admin");
+		username.sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		
+	}
+ 
+}
---
+++ Project_Jan_2026/src/test/java/SelinumPac/TC_WindowHandle.java
@@ -0,0 +1,27 @@
+package SelinumPac;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC_WindowHandle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		https://letcode.in/window
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://letcode.in/window");
+		String wh=driver.getWindowHandle();
+		System.out.println(wh);
+		driver.findElement(By.id("multi")).click();
+		System.out.println("After clicking multi url:"+driver.getCurrentUrl());
+		for(String cw:driver.getWindowHandles())
+		{
+			driver.switchTo().window(cw);
+			System.out.println("After switching url:"+driver.getCurrentUrl());
+		}
+	}
+}
